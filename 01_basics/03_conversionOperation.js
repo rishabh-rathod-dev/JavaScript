5c2a3f902fa3e3a0331520ac9f6f24c8c4090d10
rishabh-rathod-dev <31653275+rishabh-rathod-dev@users.noreply.github.com>
2023-09-06 19:24:38 +0000

    Conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc";
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueIsNumber = Number(score);
+// console.log(typeof(valueIsNumber));
+// console.log(valueIsNumber);
+
+// "33" => 33
+// "33abc" => NaN type is number
+// true => 1; false => 0;
+
+let isLoggedIn = "Ram";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false; 
+// "Ram" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
