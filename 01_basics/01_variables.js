20f8df72ee9abc8bc061bd65390c11bdef2936fc
rishabh-rathod-dev <31653275+rishabh-rathod-dev@users.noreply.github.com>
2023-09-06 19:07:09 +0000

    Data Types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1234;
+let accountEmail = "rishabh@email.com";
+var accountPassword = "12345@abC";
+accountCity = "Noida";
+let accountState;
+// accountId = 2 // not allowed
+accountEmail = "rishu@email.com";
+accountPassword = "abC@12345";
+accountCity = "Delhi";
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as newer version.
+// alert( 3+3) // we are using node js not browser
+console.log(3
+     +
+     3); // code readability shoud be high 
+console.log("Ram Ram");
+
+let name = "Ram";
+let age = 25;
+let isLoggedIn = true;
+let state ;
+
+// number => 2 to power 53
+// bigint 
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique data type
+
+// object 
+console.log(typeof undefined) // undefined
+console.log(typeof null) // Object
